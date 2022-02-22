40d615c22d15bad182d65c9932469cdf9c57d9db
snatchstin <ashtin.evans@elongate.cc>
2022-02-21 17:34:58 -0800

    sdf

--- src/index.js
+++ src/index.js
@@ -3,42 +3,26 @@ import Phaser, { CANVAS } from 'phaser';
 import PlayScene from './PlayScene';
 import PreloadScene from './PreloadScene';
 
-var width = 1000
-var height = 340
 
 
 const config = {
     type: Phaser.AUTO,
-    width,
-    height,
     pixelArt: true,
     transparent: true,
     scale: {
-        parent: CANVAS,
-
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
-
-        min: {
-            width: 600,
-            height: 340,
-        },
-
-        max: {
-            width: 1200,
-            height: 340
-        },
-
-        zoom: 1,
+        parent: 'top',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1000,
+        height: 340
     },
     physics: {
         default: 'arcade',
         arcade: {
-            debug: false
+            debug: true
         }
     },
-    scene: [PreloadScene, PlayScene],
-    // autoRound: false
+    scene: [PreloadScene, PlayScene]
 };
 
 new Phaser.Game(config);
