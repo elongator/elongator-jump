b66525169f4133b86edda8250ec23f6b4aeccc05
snatchstin <ashtin.evans@elongate.cc>
2022-02-21 18:25:31 -0800

    a

--- src/index.js
+++ src/index.js
@@ -3,55 +3,66 @@ import Phaser, { CANVAS } from 'phaser';
 import PlayScene from './PlayScene';
 import PreloadScene from './PreloadScene';
 
+function resizeApp() {
+    // Width-height-ratio of game resolution
+    // Replace 360 with your game width, and replace 640 with your game height
+    let game_ratio = 360 / 640;
 
-function resize() {
-    var canvas = document.querySelector("canvas");
-    var windowWidth = window.innerWidth;
-    var windowHeight = window.innerHeight;
-    var windowRatio = windowWidth / windowHeight;
-    var gameRatio = game.config.width / game.config.height;
+    // Make div full height of browser and keep the ratio of game resolution
+    let div = document.getElementById('phaser-app');
+    div.style.width = (window.innerHeight * game_ratio) + 'px';
+    div.style.height = window.innerHeight + 'px';
 
-    if (windowRatio < gameRatio) {
-        canvas.style.width = windowWidth + "px";
-        canvas.style.height = (windowWidth / gameRatio) + "px";
-    } else {
-        canvas.style.width = (windowHeight * gameRatio) + "px";
-        canvas.style.height = windowHeight + "px";
-    }
+    // Check if device DPI messes up the width-height-ratio
+    let canvas = document.getElementsByTagName('canvas')[0];
+
+    let dpi_w = parseInt(div.style.width) / canvas.width;
+    let dpi_h = parseInt(div.style.height) / canvas.height;
+
+    let height = window.innerHeight * (dpi_w / dpi_h);
+    let width = height * game_ratio;
+
+    // Scale canvas	
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
 }
 
+window.addEventListener('resize', resizeApp);
+
 const config = {
-    parent: CANVAS,
+    // parent: "container",
     width: 1000,
     height: 340,
     type: Phaser.AUTO,
     pixelArt: true,
     transparent: true,
-    scale: {
-        parent: CANVAS,
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
+    // scale: {
+    //     parent: "container",
+    //     mode: Phaser.Scale.FIT,
+    //     autoCenter: Phaser.Scale.CENTER_BOTH,
 
-        min: {
-            width: 500,
-            height: 340
-        },
+    //     min: {
+    //         width: 500,
+    //         height: 340
+    //     },
 
-        max: {
-            width: 1000,
-            height: 340
-        },
+    //     max: {
+    //         width: 1000,
+    //         height: 340
 
-        zoom: 1,
-    },
+    //     },
+
+    //     zoom: 1,
+    // },
     physics: {
         default: 'arcade',
         arcade: {
             debug: true
         }
     },
-    autoRound: false,
+    // autoRound: false,
     scene: [PreloadScene, PlayScene]
+
 };
 
 new Phaser.Game(config);
